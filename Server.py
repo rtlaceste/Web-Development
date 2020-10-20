d1a1377278e3b57b103d17b15f3f780e5c2c8c92
rtlaceste <73194494+rtlaceste@users.noreply.github.com>
2020-10-20 11:51:06 -0700

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thank_you.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong, please try again'
+
+# TO RUN FLASK
+# 1) In Terminal (below), input (SET FLASK_ENV=development)
+# 2) SET FLASK_APP=Server.py (or name of .py file) ##NO SPACES
+# 3) flask run
